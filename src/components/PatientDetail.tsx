2996dc3286a46c9a7f826bdf0a5dd0cfc808d7ce
martapastorperez <martapastorperez97@gmail.com>
2024-08-08 17:41:22 +0200

    feat: update patient

--- src/components/PatientDetail.tsx
+++ src/components/PatientDetail.tsx
@@ -8,7 +8,7 @@ type patientDetailProps={
 
 export const PatientDetail = ({patient}:patientDetailProps) => {
 
-    const {deletePatient}=usePatientStore()
+    const {deletePatient,getPatientId}=usePatientStore()
 
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
@@ -23,7 +23,7 @@ export const PatientDetail = ({patient}:patientDetailProps) => {
                 <button
                     type="button"
                     className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
-                   
+                    onClick={()=>getPatientId(patient.id)}
                 >Editar</button>
 
                 <button
