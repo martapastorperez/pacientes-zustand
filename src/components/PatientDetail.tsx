9d844aad14b618dc55ff52491032d49f9ce12856
martapastorperez <martapastorperez97@gmail.com>
2024-08-08 16:45:37 +0200

    feat: remove patient

--- src/components/PatientDetail.tsx
+++ src/components/PatientDetail.tsx
@@ -1,3 +1,4 @@
+import { usePatientStore } from "../store"
 import { Patient } from "../types"
 import { PatientDetailItem } from "./PatientDetailItem"
 
@@ -6,6 +7,9 @@ type patientDetailProps={
 }
 
 export const PatientDetail = ({patient}:patientDetailProps) => {
+
+    const {deletePatient}=usePatientStore()
+
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
         <PatientDetailItem label="ID" data={patient.id} />
@@ -25,7 +29,7 @@ export const PatientDetail = ({patient}:patientDetailProps) => {
                 <button
                     type="button"
                     className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
-                   
+                    onClick={()=>deletePatient(patient.id)}
                 >Eliminar</button>
             </div>
     </div>
